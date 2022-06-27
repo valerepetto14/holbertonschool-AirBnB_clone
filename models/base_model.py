734f28e0b5adbb728f1af80cbf677bd6d9d07571
santiago neira <santiagoneira52@gmail.com>
2022-06-27 17:56:57 +0000

    set time of creation with datetime

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,19 @@
 """
 Clase base:
 - vamos a definir todos los atributos/metodos comunes
-- de las que van a heredar distintas clases
+- de las que van a heredar distintas sub_clases
 """
+import models
 from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
     """
-    clase base para todos los modelos(objects)
+    clase base para todos los modelos
     """
     def __init__(self):
+        """initialization"""
         self.id = str(uuid4)
-
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()

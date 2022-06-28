bfc9193bb648deabf0df5b5c7349370d8ffd8ed4
santiago neira <santiagoneira52@gmail.com>
2022-06-28 04:13:33 +0000

    creating filestorage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class FileStorage:
+- serializa las Instancias en un 'archivo' JSON
+- deserializa el 'archivo' JSON a Instancias
+"""
+import json
+
+
+class FileStorage:
+    """
+    serializar a un archivo/deserializar de un archivo
+    """
+    

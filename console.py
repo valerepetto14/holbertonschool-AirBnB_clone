f1037ea43cd45cee81294fe6cffe6f5233bef526
santiago neira <santiagoneira52@gmail.com>
2022-06-29 03:53:42 +0000

    doing create command on the console

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 """
 programm console
 """
-
 from models.base_model import BaseModel
 import json
 import cmd
@@ -10,8 +9,8 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """def class cmd"""
-
     prompt = '(hbnb)'
+
     def do_quit(self, line):
         """def exit"""
         exit()
@@ -20,8 +19,31 @@ class HBNBCommand(cmd.Cmd):
         """def EOF"""
         exit()
 
+    def do_emptyline(self):
+        """linea vacia no hacemos nada"""
+        pass
+
     def do_create(self, line):
-        """def create"""
-    
+        """create a new instance of BaseModel"""
+        class_val = ["BaseModel"]
+        args = line.split()
+        if line == "" or line is None or len(args) < 1:
+            print("** class name missing **")
+            return
+        clase = args[0]
+        if clase not in class_val:
+            print("** class doesn't exist **")
+            return
+        """
+        evalua como código una cadena
+        si el comando existe lo ejecuto
+        """
+        if clase in class_val:
+            obj = eval(clase)()
+            """saves it (to the JSON file) and prints the id"""
+            obj.save()
+            print(obj.id)
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

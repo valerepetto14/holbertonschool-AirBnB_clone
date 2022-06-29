b2e53f28ba43a37f2b6f227bae9853b5fe6c2531
santiago neira <santiagoneira52@gmail.com>
2022-06-29 04:11:44 +0000

    add one test to console

---
+++ tests/test_console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+TEST CONSOLE
+"""
+from console import HBNBCommand
+from datetime import datetime
+import unittest
+from time import sleep
+import json
+from models.base_model import BaseModel
+
+
+class Test_console(unittest.TestCase):
+    """check commands"""
+    def test_prompt(self):
+        """comprobamos que la cadena del prompt sea correcta"""
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    

e89eb3d98fd3af711729919cbd3734157dc8a8a7
Santiago Neira <santiagoneira52@gmail.com>
2022-07-01 20:15:33 +0000

    add some test to class amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -11,12 +11,26 @@ from models.amenity import Amenity
 
 class test_amenity(unittest.TestCase):
     """Casos de prueba para la clase Amenity"""
+    def test_attr(self):
+        """chequeamos existencia de atributo"""
+        obj = Amenity()
+        self.assertTrue(hasattr(obj, "name"))
+        self.assertEqual(obj.name, "")
+        obj.name = "Naruto"
+        self.assertEqual(obj.name, "Naruto")
+    
     def test_instances_attr(self):
         """chequeamos attr(type, value) y confirmamos procedencia"""
         obj = Amenity()
         self.assertTrue(issubclass(Amenity, BaseModel))
         self.assertEqual(obj.name, "")
         self.assertEqual(type(obj.name), str)
+
+    def test_dif_id(self):
+        """Chequeamos cambios de id"""
+        obj = Amenity()
+        other = Amenity()
+        self.assertNotEqual(obj, other)
 
 if __name__ == '__main__':
   unittest.main()

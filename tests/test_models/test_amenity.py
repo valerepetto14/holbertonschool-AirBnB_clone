f04bae643a2144d9618bcab5d99c430616011dec
Santiago Neira <santiagoneira52@gmail.com>
2022-07-01 07:25:09 +0000

    some test to amenity classs

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -7,3 +7,13 @@ import unittest
 from time import sleep
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+
+class test_amenity(unittest.TestCase):
+    """Casos de prueba para la clase Amenity"""
+    def test_instances_attr(self):
+        """chequeamos attr(type, value) y confirmamos procedencia"""
+        obj = Amenity()
+        self.assertTrue(issubclass(Amenity, BaseModel))
+        self.assertEqual(obj.name, "")
+        self.assertEqual(type(obj.name), str)

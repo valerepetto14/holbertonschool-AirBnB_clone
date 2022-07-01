2548e056d7588516ebe28390caf8b338212201a7
Santiago Neira <santiagoneira52@gmail.com>
2022-07-01 05:58:18 +0000

    create public attribute class

--- models/place.py
+++ models/place.py
@@ -25,6 +25,16 @@ class Place(BaseModel):
     """Clase Place
         -Atributos publicos de clase
     """
+    city_id = ""
+    user_id = ""
     name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
 
     def __init__(self):

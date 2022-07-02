20469d099f9d23c3dfa4ccaa028a18410152a140
Santiago Neira <santiagoneira52@gmail.com>
2022-07-02 21:22:17 +0000

    test tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,12 +6,23 @@ from datetime import datetime
 import unittest
 from time import sleep
 import json
-import models
 from models.engine.file_storage import FileStorage
 
 
 class test_fileStorage(unittest.TestCase):
-    """test FileStorage"""
+    """Test FileStorage Class"""
+    def test_instances(self):
+        """chequeamos instantation"""
+        test = FileStorage()
+
+    def test_docs(self):
+        """chequeamos documentacion"""
+        self.assertIsInstance(test, FileStorage)
+        self.assertIsNotNone(FileStorage.new.__doc__)        
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
 
     if __name__ == '__main__':
         unittest.main()

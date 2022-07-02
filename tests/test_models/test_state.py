2be7e5b65c633346d58df91ec74e6d230be6f5e0
Santiago Neira <santiagoneira52@gmail.com>
2022-07-02 06:21:16 +0000

    add some tests to state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -11,6 +11,19 @@ from models.state import State
 
 class test_State():
     """Test State class"""
+    def test_attr(self):
+        """test existencia atributo"""
+        obj = State()
+        obj.name = ""
+        self.assertTrue(hasattr(obj, "name"))
+        self.assertEqual(obj.name, "")
+
+    def test_type_attr(self):
+        """test type de atributo"""
+        obj = State()
+        obj.name = "santi"
+        self.assertEqual(type(obj.name), str)
+
     def test_instantiation(self):
         """Chequeamos la instancia con y sin argumentos"""
         self.assertIs(State, type(State(name="Naruto")))

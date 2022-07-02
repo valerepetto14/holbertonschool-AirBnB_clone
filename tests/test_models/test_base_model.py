f6f7d4e9f6308bccae6c07afa03672eb2f68aee1
valentin repetto <valentinrepetto12345@gmail.com>
2022-07-02 18:16:18 +0000

    change in base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,122 +1,101 @@
 #!/usr/bin/python3
 """
-TEST OF CLASS BASE
+Test suite for Base class --> Unittest
+--> run with python3 -m unittest discover tests
+or --> run with python3 -m unittest tests/test_models/test_base_model.py
+- All your test files and folders should start with test_
 """
-from datetime import datetime
+import models
 import unittest
-from time import sleep
 import json
+from datetime import datetime
 from models.base_model import BaseModel
 
 
-class test_base_model(unittest.TestCase):
-    """def class Testbasemodel"""
-    def test_instancias(self):
-        """checkear si se crearon las instancias"""
-        obj1 = BaseModel()
-        self.assertTrue(hasattr(obj1, "id"))
-        self.assertTrue(hasattr(obj1, "created_at"))
-        self.assertTrue(hasattr(obj1, "updated_at"))
+class test_Base(unittest.TestCase):
+    """
+    Test suite for Base class.
+    """
+    def test_documentation(self):
+        """
+        Test if there is documentation
+        """
+        self.assertTrue(len(BaseModel.__doc__) > 0)
+        self.assertTrue(len(BaseModel.save.__doc__) > 0)
+        self.assertTrue(len(BaseModel.to_dict.__doc__) > 0)
+        self.assertTrue(len(BaseModel.__str__.__doc__) > 0)
 
-    def test_id(self):
-        """2 instancias deben tener diferente id"""
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        self.assertNotEqual(obj1.id, obj2.id)
-        self.assertFalse(obj1.id == obj2.id)
+    def test_id_assignment(self):
+        """
+        Test assignment of UUID is of type string and len is equal to 36
+        A UUID is made up of hex digits  (4 chars each) along with 4 “-”
+        symbols, which make its length equal to 36 characters.
+        """
+        test = BaseModel()
+        self.assertIsInstance(test.id, str)
+        self.assertEqual(len(test.id), 36)
 
-    def test_data_dif(self):
-        """crear 2 instancias en diferentes tiempos"""
-        obj1 = BaseModel()
-        sleep(0.1)
-        obj2 = BaseModel()
-        self.assertNotEqual(obj1.created_at, obj2.created_at)
+    def test_instance_of_BaseModel(self):
+        """
+        Test instantiation of a BaseModel class
+        """
+        test = BaseModel()
+        self.assertEqual(type(test), BaseModel)
+        self.assertTrue(hasattr(test, "id"))
+        self.assertTrue(hasattr(test, "created_at"))
+        self.assertTrue(hasattr(test, "updated_at"))
 
-    def test_date(self):
-        """test date"""
-        obj1 = BaseModel()
-        self.assertEqual(obj1.created_at, obj1.updated_at)
+    def test_instance_with_kwarg(self):
+        """
+        Test instantation with kwarg
+        """
+        test = BaseModel(name="Marce y Agus")
+        self.assertTrue(hasattr(test, "name"))
 
-    def test_date_types(self):
-        """chequeamos tipos date"""
-        obj = BaseModel()
-        self.assertEqual(type(obj.created_at), datetime)
-        self.assertEqual(type(obj.updated_at), datetime)
+    def test_created_at(self):
+        """
+        Test that when creating an instance of BaseModel "datetime" is assigned
+        correctly to the public instance attribute: "created_at" and is of type
+        "datetime".
+        Use of function "hasattr" to check if the attribute was created as it
+        should be.
+        """
+        test = BaseModel()
+        self.assertTrue(hasattr(test, "created_at"))
+        self.assertEqual(type(test.created_at), datetime)
 
-    def test_format_date(self):
-        """chequeamos el formato de date"""
-        obj = BaseModel()
-        dic = obj.to_dict()
-        self.assertEqual(type(dic["created_at"]), str)
-        self.assertEqual(type(dic["updated_at"]), str)
+    def test_updated_at(self):
+        """
+        Test that when creating an instance of BaseModel "datetime" is assigned
+        correctly to the public instance attribute: "updated_at" and is of type
+        "datetime".
+        Use of function "hasattr" to check if the attribute was created as it
+        should be.
+        """
+        test = BaseModel()
+        self.assertTrue(hasattr(test, "updated_at"))
+        self.assertEqual(type(test.updated_at), datetime)
 
     def test_save(self):
-        """test save"""
-        obj1 = BaseModel()
-        obj1.save()
-        self.assertNotEqual(obj1.created_at, obj1.updated_at)
+        """
+        Test save model: storage created time and updated time
+        """
+        test = BaseModel()
+        test.save()
+        self.assertNotEqual(test.created_at, test.updated_at)
 
-    def test_save(self):
-        """chequeamos función save en 2 llamadas distintas"""
-        obj = BaseModel()
-        obj.save()
-        sleep(0.1)
-        obj.save()
-        self.assertLess(obj.created_at, obj.updated_at)
-
-    def test_dict(self):
-        """comprobar typo del update luego de to_dict()"""
-        obj1 = BaseModel()
-        dic = obj1.to_dict()
-        self.assertEqual(type(dic["updated_at"]), str)
-        self.assertEqual(type(dic["created_at"]), str)
-
-    def test_dict2(self):
-        """comprobar existencia de keys """
-        obj = BaseModel()
-        self.assertIn("id", obj.to_dict())
-        self.assertIn("created_at", obj.to_dict())
-        self.assertIn("updated_at", obj.to_dict())
-
-    def test_dict_equal(self):
-        """comprobar igualdad de attributos"""
-        obj = BaseModel()
-        self.assertEqual(type(obj.to_dict()), dict)
-
-    def test_kwarg(self):
-        """test que valida que se llene mediante un diccionario"""
-        dic = {'id': '12', 'created_at': '2017-09-28T21:03:54.052302',
-               'updated_at': '2017-09-28T21:03:54.052302'}
-        obj1 = BaseModel(**dic)
-        data1 = '%Y-%m-%dT%H:%M:%S.%f'
-        data2 = '2017-09-28T21:03:54.052302'
-
-        self.assertEqual(obj1.id, "12")
-        self.assertEqual(obj1.created_at, datetime.strptime(data2, data1))
-        self.assertEqual(obj1.updated_at, datetime.strptime(data2, data1))
-        self.assertEqual(type(dic["created_at"]), str)
-
-    def test_kwargs_more(self):
-        """chequeamos que este creando bien los atributos con kwargs"""
-        obj = BaseModel(name="Naruto")
-        self.assertTrue(hasattr(obj, "name"))
-
-    def test_no_kwarg(self):
-        """chequeamos la instanciacion cunado no recibe args"""
-        self.assertEqual(BaseModel, type(BaseModel()))
+    def test_to_dict(self):
+        """
+        test to dict method
+        """
+        test = BaseModel()
+        test_dict = test.to_dict()
+        self.assertIsInstance(test_dict, dict)
 
     def test_str(self):
-        """chequeo de la str function"""
-        obj = BaseModel()
-        self.assertEqual(type(str(obj)), str)
-        # self.assertEqual(type(__str__(obj)), str)
+        """
+        test print format
+        """
 
-    def test_more_attr(self):
-        """chequeamos si se estan creando atributos"""
-        obj = BaseModel()
-        obj.name = "naruto"
-        self.assertIn("name", obj.to_dict())
-
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()

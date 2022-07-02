43597a725977ddbf01373c90ba3d50218d799642
Santiago Neira <santiagoneira52@gmail.com>
2022-07-02 06:51:06 +0000

    create some tests to City

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -11,10 +11,22 @@ from models.city import City
 
 class test_City(unittest.TestCase):
     """test to class City"""
+    def test_procedence(self):
+      """chequeamos que sea una subclase de basemodel"""
+      self.assertTrue(issubclass(City, BaseModel))
+
+    def test_attr_type(self):
+      """chequeamos tipo de atributos"""
+      obj = City()
+      self.assertEqual(type(obj.name), str)
+      self.assertEqual(type(obj.state_id), str)
+
     def tests_instances_attr(self):
         """chequeamos attr(type, value), subclass"""
         self.assertTrue('state_id' in City.__dict__)
         self.assertTrue('name' in City.__dict__)
+
+
 
 
 if __name__ == '__main__':

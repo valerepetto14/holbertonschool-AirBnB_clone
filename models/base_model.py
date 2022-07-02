238c1baea6cf228008b70ac8e9517e64156a5d50
valentin repetto <valentinrepetto12345@gmail.com>
2022-07-02 18:05:49 +0000

    change in base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,73 @@
 #!/usr/bin/python3
 """
-Clase base:
-- vamos a definir todos los atributos/metodos comunes
-- de las que van a heredar distintas sub_clases
+Module for creating a base class: BaseModel
 """
-import models
 from uuid import uuid4
 from datetime import datetime
+import models  # if we put: from models import storage -> error:circular import
 
 
 class BaseModel:
-    """
-    clase base para todos los modelos
-    """
+    """class base"""
     def __init__(self, *args, **kwargs):
-        """initialization"""
-        # print(kwargs)
-        if (kwargs is not None and len(kwargs) != 0):
+        """
+        Initialize an instance of BaseModel with the following
+        Public instance attributes:
+        id: string - assign with an uuid when an instance is created:
+        you can use uuid.uuid4() to generate unique id.
+        created_at: datetime - assign with the current datetime when an
+        instance is created
+        updated_at: datetime - assign with the current datetime when an
+        instance is created and it will be updated every time you change
+        your object
+        """
+        if kwargs and kwargs is not None:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    pass
-                elif key == 'created_at':
-                    self.created_at = datetime.strptime(value,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                elif key == 'updated_at':
-                    self.updated_at = datetime.strptime(value,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                elif key == "__class__":
+                    pass  # No le seteamos nada,solo usamos pass
                 else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.id = str(uuid4())  # convert the id to string
+            self.created_at = datetime.now()  # Use method now of datetime mod
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
         """
-        return a string representationddd
+        Should print: [<class name>] (<self.id>) <self.__dict__>
         """
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """method save"""
+        """
+        Updates the public instance attribute updated_at with the current
+        datetime.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """return a dictionary with methods/attr"""
-        diccio = self.__dict__.copy()
-        diccio["__class__"] = self.__class__.__name__
-        diccio["updated_at"] = self.updated_at.isoformat()
-        diccio["created_at"] = self.updated_at.isoformat()
-        return diccio
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance.
+        __dict__ is a pecial attribute of every module, is the dictionary
+        containing the module’s symbol table.
+        - Usamos la funcion "getattr", a la misma se le pasa la instancia
+        correspondiente (con self) y el attribute del cual se quiere
+        obtener el valor.
+        """
+
+        new_dict = {}
+
+        for attribute in self.__dict__:
+            if attribute == "created_at" or attribute == "updated_at":
+                new_dict[attribute] = getattr(self, attribute).isoformat()
+            else:
+                new_dict[attribute] = getattr(self, attribute)
+        new_dict['__class__'] = self.__class__.__name__
+        # Se agrega la key: "__class__" y se le asigna el nombre de la clase.
+        return (new_dict)

28bd5c8d89ee77a8deac418e71c816427bd3f479
Santiago Neira <santiagoneira52@gmail.com>
2022-07-02 06:47:47 +0000

    create more test to User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -19,11 +19,31 @@ class test_User(unittest.TestCase):
         self.assertEqual(obj.last_name, "")
         self.assertEqual(obj.email, "")
 
+    def test_attr_type(self):
+        """Test type de atributos"""
+        obj = User()
+        self.assertEqual(type(obj.password), str)
+        self.assertEqual(type(obj.first_name), str)
+        self.assertEqual(type(obj.last_name), str)
+        self.assertEqual(type(obj.email), str)
+
+    def test_attr_existence(self):
+        """Test existencia de atributos"""
+        obj = User()
+        self.assertTrue(hasattr(obj, "password"))
+        self.assertTrue(hasattr(obj, "first_name"))
+        self.assertTrue(hasattr(obj, "last_name"))
+        self.assertTrue(hasattr(obj, "email"))
+
     def test_dif_id(self):
         """Test difrentes id para las instancias"""
         obj = User()
         other = User()
         self.assertNotEqual(obj.id, other.id)
+
+    def test_procedence(self):
+        """chequeamos que es una subclase de BaseModel"""
+        self.assertTrue(issubclass(User, BaseModel))
 
     def test_str_representation(self):
         """Validamos el formato del str"""

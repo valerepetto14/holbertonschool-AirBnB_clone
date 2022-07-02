367aa6caa3bc7eb33868d20f0e20adcb9aed4a25
valentin repetto <valentinrepetto12345@gmail.com>
2022-07-02 20:25:12 +0000

    Revert "add test to filestoraeg"
    This reverts commit 8324be206242f355d334ee009c395f68fb5c0e6d.

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,11 +13,11 @@ class test_fileStorage(unittest.TestCase):
     """Test FileStorage Class"""
     def test_instances(self):
         """chequeamos instantation"""
-        obj = FileStorage()
-        self.assertIsInstance(obj, FileStorage)
+        test = FileStorage()
 
     def test_docs(self):
         """chequeamos documentacion"""
+        self.assertIsInstance(test, FileStorage)
         self.assertIsNotNone(FileStorage.new.__doc__)        
         self.assertIsNotNone(FileStorage.all.__doc__)
         self.assertIsNotNone(FileStorage.save.__doc__)

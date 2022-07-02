9eb1108c2cd812eae3e50362556153e22a58dea8
Santiago Neira <santiagoneira52@gmail.com>
2022-07-02 20:56:51 +0000

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,19 +10,25 @@ from models.engine.file_storage import FileStorage
 
 
 class test_fileStorage(unittest.TestCase):
-    """Test FileStorage Class"""
-    def test_instances(self):
-        """chequeamos instantation"""
-        test = FileStorage()
-        
+    """
+    File_Storage test
+    """
 
-    def test_docs(self):
-        """chequeamos documentacion"""
-        self.assertIsInstance(test, FileStorage)
-        self.assertIsNotNone(FileStorage.new.__doc__)        
+    def test_documentation(self):
+        """
+        test documentation
+        """
         self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
         self.assertIsNotNone(FileStorage.save.__doc__)
         self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_is_an_instance(self):
+        """
+        test instantation
+        """
+        test = FileStorage()
+        self.assertIsInstance(test, FileStorage)
 
 
     if __name__ == '__main__':

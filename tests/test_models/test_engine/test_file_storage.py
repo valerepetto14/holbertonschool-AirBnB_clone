3e9b0a72062880b86117ce4906cadddb637b8027
Santiago Neira <santiagoneira52@gmail.com>
2022-07-02 21:14:31 +0000

    test tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,23 +6,36 @@ from datetime import datetime
 import unittest
 from time import sleep
 import json
+import models
 from models.engine.file_storage import FileStorage
 
 
 class test_fileStorage(unittest.TestCase):
-    """
-    File_Storage test
-    """
-
-    def test_documentation(self):
-        """
-        test documentation
-        """
+    """Test FileStorage Class"""
+    def test_docs(self):
+        """chequeamos documentacion"""
+        self.assertIsNotNone(FileStorage.new.__doc__)        
         self.assertIsNotNone(FileStorage.all.__doc__)
-        self.assertIsNotNone(FileStorage.new.__doc__)
         self.assertIsNotNone(FileStorage.save.__doc__)
         self.assertIsNotNone(FileStorage.reload.__doc__)
 
+
+    def test_classes(self):
+        """check classes"""
+        self.assertIsInstance(models.engine.file_storage.FileStorage(),
+                              models.engine.file_storage.FileStorage)
+
+    def test_all(self):
+        """check all"""
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().all)
+
+    def test_new(self):
+        """check if new method is working"""
+
+        self.assertIsNotNone(models.engine.file_storage.FileStorage().new)
+
+    def test_save(self):
+        """check if save method is working"""
 
     if __name__ == '__main__':
         unittest.main()

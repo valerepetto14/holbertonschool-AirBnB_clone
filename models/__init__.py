a973f3d4b505c54390e7ebaad4a302dee8c6b557
valentin repetto <valentinrepetto12345@gmail.com>
2022-07-02 18:28:38 +0000

    change in base model

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """
-__init__.py can just be an empty file, but it can also execute initialization
-code for the package.
-Here we are initializing "storage" to create a unique FileStorage
-instance for our application.
+init
 """
+
 from models.engine.file_storage import FileStorage
 
-
-storage = FileStorage()  # Create storage as an instance of "FileStorage"
-storage.reload()  # Call reload method on "storage" instance
+storage = FileStorage()
+storage.reload()
